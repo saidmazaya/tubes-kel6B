5ef96cabd956fc67372fe65ec22f718b13bd6eaf
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-04-28 16:23:06 +0700

    ngubah databasenya

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('id');
             $table->string('username', 50);
-            $table->string('user_email')->unique();
-            $table->string('user_bio', 160);
-            $table->text('user_image');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('bio', 160);
+            $table->text('image');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2023_04_28_075100_create_articles_table.php
+++ database/migrations/2023_04_28_075100_create_articles_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->id('article_id');
-            $table->string('article_title');    
-            $table->string('article_description', 300);
-            $table->text('article_content');
-            $table->text('article_image');
-            $table->unsignedInteger('article_sum');
+            $table->id('id');
+            $table->string('title');    
+            $table->string('description', 300);
+            $table->text('content');
+            $table->text('image');
+            $table->unsignedInteger('sum');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
             $table->timestamps();
