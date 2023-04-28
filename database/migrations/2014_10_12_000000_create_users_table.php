d596ff2c1f32035db91d1e83d8e4555395f9d6e5
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-04-28 16:17:56 +0700

    tabel article dan user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->id('id');
+            $table->string('username', 50);
+            $table->string('user_email')->unique();
+            $table->string('user_bio', 160);
+            $table->text('user_image');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2023_04_28_075100_create_articles_table.php
+++ database/migrations/2023_04_28_075100_create_articles_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->id();
+            $table->id('article_id');
+            $table->string('article_title');    
+            $table->string('article_description', 300);
+            $table->text('article_content');
+            $table->text('article_image');
+            $table->unsignedInteger('article_sum');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
             $table->timestamps();
         });
     }
