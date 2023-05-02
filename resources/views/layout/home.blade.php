9f031f35e367af439a3066e57c6d56f189696243
Nurul Aini <nrlaini04@gmail.com>
2023-05-02 12:15:23 +0700

    ubah href

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -8,7 +8,7 @@
   <title>PREMIUM | @yield('title')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="css/main.css">
   @stack('css')
 </head>
 
@@ -19,7 +19,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
   </script>
-  <script src="signin.js"></script>
+  <script src="js/main.js"></script>
   @stack('js')
 </body>
 
