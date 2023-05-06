335acb1c2ce9894d34e7d22a286b1f70aa43c557
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-06 08:30:30 +0700

    nambah + perbarui

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('id');
-            $table->string('username', 50);
+            $table->string('username', 30);
+            $table->string('name', 50);
+            $table->string('password', 16);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('bio', 160);
-            $table->text('image');
+            $table->string('image')->nullable();
+            $table->string('bio')->nullable();
+            $table->text('about')->nullable();
+            $table->string('link');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_05_06_011803_create_comment_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment_lists', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->enum('status', ['Published', 'Pending'])->default('Pending');
+            $table->string('link');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->unsignedBigInteger('article_list_id');
+            $table->foreign('article_list_id')->references('id')->on('article_lists')->onDelete('restrict');
+            $table->unsignedBigInteger('clap_list_id');
+            $table->foreign('clap_list_id')->references('id')->on('clap_lists')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_lists');
+    }
+};
