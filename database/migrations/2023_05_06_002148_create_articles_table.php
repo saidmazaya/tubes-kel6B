83e412dde8ac22501f2613aa9e4bf4e1a06ae6ec
Said Muhammad Mazaya <117622294+saidmazaya@users.noreply.github.com>
2023-05-06 08:32:32 +0700

    ngubah + nambah

---
+++ database/migrations/2023_05_06_002148_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->integer('duration');
+            $table->string('link');
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('restrict');
+            $table->enum('status', ['Draft', 'Pending', 'Published'])->default('Draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
