eb100ad26dea7e99e93a8ade96caa1089e6bedf2
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-08 10:43:07 +0700

    Model

---
+++ app/Models/ArticleList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleList extends Model
+{
+    use HasFactory;
+}
