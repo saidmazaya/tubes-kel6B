c0ac8432b9f57e706da28dab152621247c2ba468
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-08 10:35:50 +0700

    nambah

---
+++ resources/views/home/ourstory.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.home')
+@section('title', 'Our Story')
+
+@section('content')
+<div class="container mt-3">
+  <div class="header">
+    <h1>Every idea needs a Premium</h1>
+    <hr class="my-5">
+    <p>The best ideas can change who we are. Medium is where those ideas take shape, take off, and spark powerful
+      conversations. We’re an open platform where over 100 million readers come to find insightful and dynamic thinking.
+      Here, expert and undiscovered voices alike dive into the heart of any topic and bring new ideas to the surface.
+      Our
+      purpose is to spread these ideas and deepen understanding of the world.</p>
+    <p>We're creating a new model for digital publishing. One that supports nuance, complexity, and vital storytelling
+      without giving in to the incentives of advertising. It’s an environment that’s open to everyone but promotes
+      substance and authenticity. And it’s where deeper connections forged between readers and writers can lead to
+      discovery and growth. Together with millions of collaborators, we’re building a trusted and vibrant ecosystem
+      fueled
+      by important ideas and the people who think about them.</p>
+  </div>
+</div>
+@endsection
