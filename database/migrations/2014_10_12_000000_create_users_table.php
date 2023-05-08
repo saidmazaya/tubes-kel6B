e3e872b6689699a124b31e5113a7ed7fef4929a5
Said Muhammad Mazaya <117622294+saidmazaya@users.noreply.github.com>
2023-05-08 11:57:36 +0700

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('username', 30);
+            $table->string('name', 50);
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('image')->nullable();
+            $table->string('bio')->nullable();
+            $table->text('about')->nullable();
+            $table->string('link');
+            $table->integer('role');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
