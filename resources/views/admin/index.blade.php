ca65f3773e8b5590b5634f0bfeb34939b2dcfedb
Nurul Aini <nrlaini04@gmail.com>
2023-05-15 10:01:28 +0700

    nambah

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,300 @@
+@extends('admin.layout.main')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+  <!-- partial -->
+  <div class="container-fluid page-body-wrapper">
+    <!-- partial:partials/_settings-panel.html -->
+    {{-- <div class="theme-setting-wrapper">
+      <div id="settings-trigger"><i class="ti-settings"></i></div>
+      <div id="theme-settings" class="settings-panel">
+        <i class="settings-close ti-close"></i>
+        <p class="settings-heading">SIDEBAR SKINS</p>
+        <div class="sidebar-bg-options selected" id="sidebar-light-theme">
+          <div class="img-ss rounded-circle bg-light border me-3"></div>Light
+        </div>
+        <div class="sidebar-bg-options" id="sidebar-dark-theme">
+          <div class="img-ss rounded-circle bg-dark border me-3"></div>Dark
+        </div>
+        <p class="settings-heading mt-2">HEADER SKINS</p>
+        <div class="color-tiles mx-0 px-4">
+          <div class="tiles success"></div>
+          <div class="tiles warning"></div>
+          <div class="tiles danger"></div>
+          <div class="tiles info"></div>
+          <div class="tiles dark"></div>
+          <div class="tiles default"></div>
+        </div>
+      </div>
+    </div> --}}
+    @include('admin.components.sidebar')
+    {{-- <div id="right-sidebar" class="settings-panel">
+      <i class="settings-close ti-close"></i>
+      <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" id="todo-tab" data-bs-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="chats-tab" data-bs-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
+        </li>
+      </ul>
+      <div class="tab-content" id="setting-content">
+        <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
+          <div class="add-items d-flex px-3 mb-0">
+            <form class="form w-100">
+              <div class="form-group d-flex">
+                <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
+                <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
+              </div>
+            </form>
+          </div>
+          <div class="list-wrapper px-3">
+            <ul class="d-flex flex-column-reverse todo-list">
+              <li>
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input class="checkbox" type="checkbox">
+                    Team review meeting at 3.00 PM
+                  </label>
+                </div>
+                <i class="remove ti-close"></i>
+              </li>
+              <li>
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input class="checkbox" type="checkbox">
+                    Prepare for presentation
+                  </label>
+                </div>
+                <i class="remove ti-close"></i>
+              </li>
+              <li>
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input class="checkbox" type="checkbox">
+                    Resolve all the low priority tickets due today
+                  </label>
+                </div>
+                <i class="remove ti-close"></i>
+              </li>
+              <li class="completed">
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input class="checkbox" type="checkbox" checked>
+                    Schedule meeting for next week
+                  </label>
+                </div>
+                <i class="remove ti-close"></i>
+              </li>
+              <li class="completed">
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input class="checkbox" type="checkbox" checked>
+                    Project review
+                  </label>
+                </div>
+                <i class="remove ti-close"></i>
+              </li>
+            </ul>
+          </div>
+          <h4 class="px-3 text-muted mt-5 fw-light mb-0">Events</h4>
+          <div class="events pt-4 px-3">
+            <div class="wrapper d-flex mb-2">
+              <i class="ti-control-record text-primary me-2"></i>
+              <span>Feb 11 2018</span>
+            </div>
+            <p class="mb-0 font-weight-thin text-gray">Creating component page build a js</p>
+            <p class="text-gray mb-0">The total number of sessions</p>
+          </div>
+          <div class="events pt-4 px-3">
+            <div class="wrapper d-flex mb-2">
+              <i class="ti-control-record text-primary me-2"></i>
+              <span>Feb 7 2018</span>
+            </div>
+            <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
+            <p class="text-gray mb-0 ">Call Sarah Graves</p>
+          </div>
+        </div>
+        <!-- To do section tab ends -->
+        <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
+          <div class="d-flex align-items-center justify-content-between border-bottom">
+            <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
+            <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 fw-normal">See All</small>
+          </div>
+          <ul class="chat-list">
+            <li class="list active">
+              <div class="profile"><img src="images/faces/face1.jpg" alt="image"><span class="online"></span></div>
+              <div class="info">
+                <p>Thomas Douglas</p>
+                <p>Available</p>
+              </div>
+              <small class="text-muted my-auto">19 min</small>
+            </li>
+            <li class="list">
+              <div class="profile"><img src="images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
+              <div class="info">
+                <div class="wrapper d-flex">
+                  <p>Catherine</p>
+                </div>
+                <p>Away</p>
+              </div>
+              <div class="badge badge-success badge-pill my-auto mx-2">4</div>
+              <small class="text-muted my-auto">23 min</small>
+            </li>
+            <li class="list">
+              <div class="profile"><img src="images/faces/face3.jpg" alt="image"><span class="online"></span></div>
+              <div class="info">
+                <p>Daniel Russell</p>
+                <p>Available</p>
+              </div>
+              <small class="text-muted my-auto">14 min</small>
+            </li>
+            <li class="list">
+              <div class="profile"><img src="images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
+              <div class="info">
+                <p>James Richardson</p>
+                <p>Away</p>
+              </div>
+              <small class="text-muted my-auto">2 min</small>
+            </li>
+            <li class="list">
+              <div class="profile"><img src="images/faces/face5.jpg" alt="image"><span class="online"></span></div>
+              <div class="info">
+                <p>Madeline Kennedy</p>
+                <p>Available</p>
+              </div>
+              <small class="text-muted my-auto">5 min</small>
+            </li>
+            <li class="list">
+              <div class="profile"><img src="images/faces/face6.jpg" alt="image"><span class="online"></span></div>
+              <div class="info">
+                <p>Sarah Graves</p>
+                <p>Available</p>
+              </div>
+              <small class="text-muted my-auto">47 min</small>
+            </li>
+          </ul>
+        </div>
+        <!-- chat tab ends -->
+      </div>
+    </div> --}}
+    <!-- partial -->
+
+    <!-- partial -->
+    <div class="main-panel">
+      <div class="content-wrapper">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="home-tab">
+              <div class="d-sm-flex align-items-center justify-content-between border-bottom">
+                {{-- <div>
+                  <div class="btn-wrapper">
+                    <a href="#" class="btn btn-otline-dark align-items-center"><i class="icon-share"></i> Share</a>
+                    <a href="#" class="btn btn-otline-dark"><i class="icon-printer"></i> Print</a>
+                    <a href="#" class="btn btn-primary text-white me-0"><i class="icon-download"></i> Export</a>
+                  </div>
+                </div> --}}
+              </div>
+              <div class="tab-content tab-content-basic">
+                <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="statistics-details d-flex align-items-center justify-content-between">
+                        <div>
+                          <p class="statistics-title">Bounce Rate</p>
+                          <h3 class="rate-percentage">32.53%</h3>
+                          <p class="text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>-0.5%</span></p>
+                        </div>
+                        <div>
+                          <p class="statistics-title">Page Views</p>
+                          <h3 class="rate-percentage">7,682</h3>
+                          <p class="text-success d-flex"><i class="mdi mdi-menu-up"></i><span>+0.1%</span></p>
+                        </div>
+                        <div>
+                          <p class="statistics-title">New Sessions</p>
+                          <h3 class="rate-percentage">68.8</h3>
+                          <p class="text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>68.8</span></p>
+                        </div>
+                        <div class="d-none d-md-block">
+                          <p class="statistics-title">Avg. Time on Site</p>
+                          <h3 class="rate-percentage">2m:35s</h3>
+                          <p class="text-success d-flex"><i class="mdi mdi-menu-down"></i><span>+0.8%</span></p>
+                        </div>
+                        <div class="d-none d-md-block">
+                          <p class="statistics-title">New Sessions</p>
+                          <h3 class="rate-percentage">68.8</h3>
+                          <p class="text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>68.8</span></p>
+                        </div>
+                        <div class="d-none d-md-block">
+                          <p class="statistics-title">Avg. Time on Site</p>
+                          <h3 class="rate-percentage">2m:35s</h3>
+                          <p class="text-success d-flex"><i class="mdi mdi-menu-down"></i><span>+0.8%</span></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-12 d-flex flex-column">
+                      <div class="row flex-grow">
+                        <div class="col-12 grid-margin stretch-card">
+                          <div class="card card-rounded">
+                            <div class="card-body">
+                              <div class="d-sm-flex justify-content-between align-items-start">
+                                <div>
+                                  <h4 class="card-title card-title-dash">Market Overview</h4>
+                                  <p class="card-subtitle card-subtitle-dash">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                                </div>
+                                <div>
+                                  <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle toggle-dark btn-lg mb-0 me-0" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> This month </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                                      <h6 class="dropdown-header">Settings</h6>
+                                      <a class="dropdown-item" href="#">Action</a>
+                                      <a class="dropdown-item" href="#">Another action</a>
+                                      <a class="dropdown-item" href="#">Something else here</a>
+                                      <div class="dropdown-divider"></div>
+                                      <a class="dropdown-item" href="#">Separated link</a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="d-sm-flex align-items-center mt-1 justify-content-between">
+                                <div class="d-sm-flex align-items-center mt-4 justify-content-between">
+                                  <h2 class="me-2 fw-bold">$36,2531.00</h2>
+                                  <h4 class="me-2">USD</h4>
+                                  <h4 class="text-success">(+1.37%)</h4>
+                                </div>
+                                <div class="me-3">
+                                  <div id="marketing-overview-legend"></div>
+                                </div>
+                              </div>
+                              <div class="chartjs-bar-wrapper mt-3">
+                                <canvas id="marketingOverview"></canvas>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+      <!-- partial:partials/_footer.html -->
+      <footer class="footer">
+        <div class="d-sm-flex justify-content-center justify-content-sm-between">
+          <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
+          <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2023. All rights reserved.</span>
+        </div>
+      </footer>
+      <!-- partial -->
+    </div>
+    <!-- main-panel ends -->
+  </div>
+  <!-- page-body-wrapper ends -->
+@endsection
---
+++ resources/views/components/navbar-main.blade.php
@@ -0,0 +1,27 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top header-inner-pages">
+    <div class="container" id="containernav">
+        <div class="navbar d-flex align-items-center justify-content-between">
+            <form method="GET" action="#" id="formNav">
+                <input type="text"  style="border-radius: 4px; width: 6cm; padding: 4px" placeholder="Search...">
+                <button type="submit" style="padding: 4px" class=""><i class="bi bi-search"></i></button>
+            </form>
+            
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="/nulis"><i class='bx bx-edit bx-sm mb-2'></i></a></li>
+                    <li><a class="nav-link scrollto" href="/notif"><i class="bx bx-bell bx-sm mb-2"></i></a></li>
+                    <li class="dropdown"><a href="#" class="nav-link scrollto mb-1"><img src="path_to_profile_image" class="profile" alt="Profile"><i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/library">Library</a></li>
+                            <li><a href="/stories">Stories</a></li>
+                            <li><a href="#">Sign Out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+        </div>
+    </div>
+</header><!-- End Header -->
