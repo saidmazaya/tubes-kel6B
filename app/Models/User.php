3e993072108527488ce4c0fcd377c62bc08e2356
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-23 14:20:53 +0700

    nambah logika follow di model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,4 +85,29 @@ class User extends Authenticatable
     {
         return $this->hasMany(ClapCommentArticle::class);
     }
+
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'mutuals', 'user_id', 'following_user_id')->withTimestamps();
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'mutuals', 'following_user_id', 'user_id')->withTimestamps();
+    }
+
+    public function follow(User $user)
+    {
+        return $this->follows()->save($user);
+    }
+
+    public function hasFollow(User $user)
+    {
+        return $this->follows()->where('following_user_id', $user->id)->exists();
+    }
+
+    public function unfollow(User $user)
+    {
+        return $this->follows()->detach($user);
+    }
 }
