e3627f82a5e8f4dbadaae32fc8c46bc528e73d8f
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-24 21:13:28 +0700

    namabah detail

--- resources/views/admin/tag/tag.blade.php
+++ resources/views/admin/tag/tag.blade.php
@@ -51,7 +51,8 @@
                                             @endphp
                                             <td>{{ $tagUseCount }}</td>
                                             <td>
-                                                <a href="{{ route('tag.edit', $data->slug) }}" class="btn-sm text-decoration-none btn-primary">Edit</a>
+                                                <a href="{{ route('tag.detail', $data->slug) }}" class="btn-sm text-decoration-none btn-info">Detail</a>
+                                                | <a href="{{ route('tag.edit', $data->slug) }}" class="btn-sm text-decoration-none btn-primary">Edit</a>
                                                 | <form class="d-inline" action="{{ route('tag.destroy', $data->id) }}" method="POST" id="deleteForm{{ $data->id }}" data-tag-id="{{ $data->id }}" data-related-articles="{{ $tagUseCount }}">
                                                     @csrf
                                                     @method('delete')
