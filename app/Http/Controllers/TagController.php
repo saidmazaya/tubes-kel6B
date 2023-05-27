ebed3bd71ba3b7e61aae107a2f882f00277c9a18
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-27 18:04:29 +0700

    buat follow tag

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Tag;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
 {
     public function show($slug)
     {
         $tagCheck = Tag::where('slug', $slug)->first();
-        
+
         if ($tagCheck) {
             $article = Article::whereHas('tags', function ($query) use ($slug) {
                 $query->where('slug', $slug);
@@ -19,7 +20,6 @@ class TagController extends Controller
                 ->where('status', 'Published')
                 ->paginate(10);
             $tag = Tag::all();
-
             $articleCount = Article::whereHas('tags', function ($query) use ($slug) {
                 $query->where('slug', $slug);
             })
@@ -33,7 +33,7 @@ class TagController extends Controller
                 ->distinct('author_id')
                 ->count('author_id');
 
-            return view('tag-detail', compact('article', 'tag', 'articleCount', 'authorCount'));
+            return view('tag-detail', compact('article', 'tag', 'articleCount', 'authorCount', 'tagCheck'));
         } else {
             abort(404);
         }
@@ -49,5 +49,17 @@ class TagController extends Controller
             ->get();
 
         return view('tag-explore', compact('tag', 'keyword'));
+    }
+
+    public function store(Request $request, Tag $tag)
+    {
+
+        if ($tag) {
+            Auth::user()->hasFollowTag($tag) ? Auth::user()->unfollowTag($tag) : Auth::user()->followTag($tag);
+
+            return back();
+        } else {
+            abort(404);
+        }
     }
 }
