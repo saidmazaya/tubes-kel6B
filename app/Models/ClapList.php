ff3a5867c3c9e3bce51c818d5b5088a0b95ad920
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-05-31 19:42:52 +0700

    nambah protected

--- app/Models/ClapList.php
+++ app/Models/ClapList.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ClapList extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'article_list_id',
+    ];
+
+    public function articleList()
+    {
+        return $this->belongsTo(ArticleList::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
