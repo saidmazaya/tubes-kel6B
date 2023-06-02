43a69cb6b7cfdff6d5b183ea52dc2b6aff5970b1
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-06-02 10:48:19 +0700

    ngapus yg gk perlu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -46,16 +46,6 @@
                         <p class="statistics-title">Total Tag</p>
                         <h3 class="rate-percentage">{{ $tagCount }}</h3>
                       </div>
-                      <div class="d-none d-md-block">
-                        <p class="statistics-title">New Sessions</p>
-                        <h3 class="rate-percentage">68.8</h3>
-                        <p class="text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>68.8</span></p>
-                      </div>
-                      <div class="d-none d-md-block">
-                        <p class="statistics-title">Avg. Time on Site</p>
-                        <h3 class="rate-percentage">2m:35s</h3>
-                        <p class="text-success d-flex"><i class="mdi mdi-menu-down"></i><span>+0.8%</span></p>
-                      </div>
                     </div>
                   </div>
                 </div>
