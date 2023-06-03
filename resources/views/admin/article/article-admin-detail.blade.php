c061d08b8570f8fedb14bcb22af3a39eb6cec35e
Nurul Aini <nrlaini04@gmail.com>
2023-06-03 10:42:16 +0700

    width

--- resources/views/admin/article/article-admin-detail.blade.php
+++ resources/views/admin/article/article-admin-detail.blade.php
@@ -69,6 +69,7 @@
 </div>
 @endsection
 @push('css')
+
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -79,3 +80,4 @@
 <link href="/assets/css/style.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 @endpush
+
